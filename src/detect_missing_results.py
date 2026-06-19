07bdf513a7135f27431176d2601e282007890aaf
Titika_ai <titikamenouali@gmail.com>
2026-06-19 16:34:25 +0200

    Update detect_missing_results.py

--- src/detect_missing_results.py
+++ src/detect_missing_results.py
@@ -1,1 +1,13 @@
+def detect_missing_results(df):
+    """
+    Detect requirements with missing results.
+    """
 
+    missing_df = df[df["result"] == "EMPTY"]
+
+    missing_requirements = missing_df["requirement"].drop_duplicates().tolist()
+
+    return {
+        "count": len(missing_requirements),
+        "missing_requirements": missing_requirements
+    }
