b385aadb4c58533e10cacffa9d4634b41ec75654
Titika_ai <titikamenouali@gmail.com>
2026-06-19 16:01:51 +0200

    Update detect_failures.py

--- src/detect_failures.py
+++ src/detect_failures.py
@@ -1,1 +1,13 @@
+def detect_failures(df):
+    """
+    Detect failed requirements.
+    """
 
+    failed_df = df[df["result"] == "FAIL"]
+
+    failed_requirements = failed_df["requirement"].tolist()
+
+    return {
+        "count": len(failed_requirements),
+        "failed_requirements": failed_requirements
+    }
